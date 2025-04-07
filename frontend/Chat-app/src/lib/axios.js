23817ed77518a638433fdc0e0973071d6d6cdd75
Ritik <ritik.kaintura007@gmail.com>
2025-04-07 12:09:19 +0530

    updates

--- frontend/Chat-app/src/lib/axios.js
+++ frontend/Chat-app/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api",
-    withCredentials: true,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5002/api"
+      : import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
